112f0232eb1bfa261d0176c1474187bb279fe046
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2022-05-06 16:08:51 -0700

    tests: add unit test for skewness()

--- tests/testthat/unit-test-utils.R
+++ tests/testthat/unit-test-utils.R
@@ -14,3 +14,15 @@ test_that("test fixStruct.lookup", {
   expect_equal(30, fixStruct.lookup("VVV"))
   expect_equal(30, fixStruct.lookup("EEE"))
 })
+
+
+context("test skewness")
+test_that("test skewness", {
+  set.seed(123)
+  alpha.seq <- c(0.1, 1, 2, 5)
+  for(a in seq_along(alpha.seq)){
+    expect_equal(2 / sqrt(alpha.seq[a]), 
+                 mean(replicate(1000, skewness(rgamma(10000,alpha.seq[a],1)))), 
+                 tolerance = 0.01)
+  }
+})
